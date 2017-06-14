f41167097afe43633cc9ad742db05c45f14603bb
Jeremy Kun <kun.jeremy@gmail.com>
2017-06-13 23:23:27 -0700

    improve fetch

--- fetch.py
+++ fetch.py
@@ -1,11 +1,24 @@
 import asyncio
+import json
 
 import aiohttp
 import async_timeout
 
 from parse import parse
 
-queue = asyncio.Queue()
+errors = {}
+data = []
+
+try:
+    with open('data.json', 'r') as infile:
+        data = json.load(infile)['nodes']
+except:
+    pass
+
+existing = set(x['id'] for x in data)
+
+sem = asyncio.BoundedSemaphore(5)
+loop = asyncio.get_event_loop()
 
 id_min = 1
 id_max = 216676
@@ -14,25 +27,46 @@ id_max = 216676
 async def fetch(session, url):
     with async_timeout.timeout(10):
         async with session.get(url) as response:
+            print('fetching {}'.format(url))
             return await response.text()
 
-async def fetch_by_id(mgp_id):
-    url = 'https://genealogy.math.ndsu.nodak.edu/id.php?id={}'.format(mgp_id)
-    async with aiohttp.ClientSession(loop=loop) as session:
+
+async def fetch_by_id(session, mgp_id):
+    async with sem:
+        url = 'https://genealogy.math.ndsu.nodak.edu/id.php?id={}'.format(mgp_id)
         raw_html = await fetch(session, url)
 
-    if 'You have specified an ID that does not exist in the database.' in raw_html:
-        print('failed id={}'.format(mgp_id))
-        return
+        if 'You have specified an ID that does not exist in the database.' in raw_html:
+            print('bad id={}'.format(mgp_id))
+            return
 
-    info_dict = parse(mgp_id, raw_html)
-    print('finished id={}'.format(mgp_id))
-    print(info_dict)
+        failed = False
+        info_dict = {}
+
+        try:
+            info_dict = parse(mgp_id, raw_html)
+        except Exception as e:
+            print('Failed to parse id={}'.format(mgp_id))
+            failed = e
+        finally:
+            if failed:
+                errors[mgp_id] = failed
+            else:
+                data.append(info_dict)
 
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(
-    asyncio.wait([
-        fetch_by_id(i) for i in range(id_min, 1 + id_max)
-    ])
-)
+async def main():
+    async with aiohttp.ClientSession(loop=loop) as session:
+        await asyncio.wait([
+            fetch_by_id(session, i) for i in range(id_min, 1 + id_max) if i not in existing
+        ])
+
+
+loop.run_until_complete(main())
+
+with open('errors.txt', 'w') as outfile:
+    for i, error in errors.items():
+        outfile.write('{},{}\n'.format(i, error))
+
+with open('data.json', 'w') as outfile:
+    json.dump({'nodes': data}, outfile)
